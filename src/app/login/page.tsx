1ce524a4f2349cd765e0b23d8e2223724c57d3c6
Aadil M. Alli <chainedraptor@gmail.com>
2025-02-23 19:11:12 -0500

    land

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,7 +6,8 @@ export default async function LoginPage() {
 
 
     const session = await auth()
-    if (session?.user) return redirect("/")
+    if (session?.user.role==="manufacturer") return redirect("/dashboard")
+    if (session?.user.role!=="product_owner") return redirect("/")
 
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
