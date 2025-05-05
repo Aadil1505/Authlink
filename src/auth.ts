f256f3cd0762051cc6431a5e34105669f7efeebd
Ethan Ramos <ethan.n.ramos@gmail.com>
2025-05-05 17:01:17 -0400

    Added the manufacturer_code to the auth schema

--- src/auth.ts
+++ src/auth.ts
@@ -1,28 +1,28 @@
-import NextAuth from "next-auth"
-import Credentials from "next-auth/providers/credentials"
-import { ZodError } from "zod"
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { ZodError } from "zod";
 // Your own logic for dealing with plaintext password strings; be careful!
-import { loginUser } from "./lib/actions/auth"
-import { signInSchema } from "./lib/schema"
- 
-export const { handlers, signIn, signOut, auth } = NextAuth({
+import { loginUser } from "./lib/actions/auth";
+import { signInSchema } from "./lib/schema";
 
+export const { handlers, signIn, signOut, auth } = NextAuth({
   callbacks: {
     async jwt({ token, user }) {
       if (user) {
         token.id = user.id;
         token.role = user.role;
         token.name = `${user.first_name} ${user.last_name}`;
-        token.profile_picture = user.profile_picture
+        token.profile_picture = user.profile_picture;
+        token.manufacturer_code = (user as any)?.manufacturer_code;
       }
       return token;
     },
     async session({ session, token }) {
       session.user.id = token.id;
       session.user.role = token.role;
-      session.user.name = token.name
-      session.user.profile_picture = token.profile_picture
-
+      session.user.name = token.name;
+      session.user.profile_picture = token.profile_picture;
+      session.user.manufacturer_code = token.manufacturer_code;
       return session;
     },
   },
@@ -34,32 +34,34 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       },
       authorize: async (credentials) => {
         try {
-          let user = null
- 
-          const { username, password } = await signInSchema.parseAsync(credentials)
- 
+          let user = null;
+
+          const { username, password } = await signInSchema.parseAsync(
+            credentials
+          );
+
           // logic to salt and hash password
           // logic to verify if the user exists
-          user = await loginUser(username, password)
-          console.log(user)
- 
+          user = await loginUser(username, password);
+          console.log(user);
+
           if (!user) {
-            console.log("user not found")
+            console.log("user not found");
             // return null
-            throw new Error("Invalid credentials.")
+            throw new Error("Invalid credentials.");
           }
- 
+
           // return JSON object with the user data
-          console.log("user info", user)
-          return user
+          console.log("user info", user);
+          return user;
         } catch (error) {
           if (error instanceof ZodError) {
-            console.log("Did not pass Zod parsing")
+            console.log("Did not pass Zod parsing");
             // Return `null` to indicate that the credentials are invalid
-            return null
+            return null;
           }
         }
       },
     }),
   ],
-})
+});
